0e8cb561dc6f426e31a6db79c3561cc9cc04fd53
divaagus <divaagustine07@gmail.com>
2019-11-22 14:58:34 +0700

    first commit

---
+++ app/src/main/java/limfiq/inc/crudmysql/ServerAPI.java
@@ -0,0 +1,9 @@
+package limfiq.inc.crudmysql;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.43.57/s5/s5/Praktikum/Mobile%20Computing%202/utsandro/crud/view_data.php";
+    public static final String URL_INSERT = "http://192.168.43.57/s5/s5/Praktikum/Mobile%20Computing%202/utsandro/crud/create_data.php";
+    public static final String URL_DELETE = "http://192.168.43.57/s5/Praktikum/Mobile%20Computing%202/utsandro/crud/delete_data.php";
+    public static final String URL_UPDATE = "http://192.168.43.57/s5/Praktikum/Mobile%20Computing%202/utsandro/crud/update_data.php";
+}
+
